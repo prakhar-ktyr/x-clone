da15b59588f3e1ba2bbe4a8f06fb30860a370001
Prakhar Katiyar <prakhar_katiyar@outlook.com>
2024-07-31 11:34:57 +0530

    chore: Update login method in AuthService to save token and user data

--- x-clone-client/src/app/services/auth.service.ts
+++ x-clone-client/src/app/services/auth.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
+import { TokenStorageService } from './token-storage.service';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -9,13 +11,32 @@ import { environment } from '../../environments/environment';
 export class AuthService {
   private apiUrl = environment.apiUrl;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private tokenStorage: TokenStorageService) {}
 
   register(user: any): Observable<any> {
     return this.http.post(`${this.apiUrl}/auth/signup`, user);
   }
 
   login(user: any): Observable<any> {
-    return this.http.post(`${this.apiUrl}/auth/login`, user);
+    return this.http.post(`${this.apiUrl}/auth/login`, user).pipe(
+      tap((response: any) => {
+        if (response.token) {
+          this.tokenStorage.saveToken(response.token);
+          this.tokenStorage.saveUser(response.user);
+        }
+      })
+    );
+  }
+
+  logout(): void {
+    this.tokenStorage.signOut();
+  }
+
+  getUser(): any {
+    return this.tokenStorage.getUser();
+  }
+
+  getToken(): string | null {
+    return this.tokenStorage.getToken();
   }
 }
