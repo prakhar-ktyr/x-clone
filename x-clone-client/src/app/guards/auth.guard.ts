75fefe4e77907cd62786e512ee4bf64fc03a77a1
Prakhar Katiyar <prakhar_katiyar@outlook.com>
2024-08-14 10:27:51 +0530

    chore: Update auth guard to navigate to home page when user is not authenticated

--- x-clone-client/src/app/guards/auth.guard.ts
+++ x-clone-client/src/app/guards/auth.guard.ts
@@ -17,7 +17,7 @@ export class AuthGuard implements CanActivate {
     if (token) {
       return true;
     } else {
-      this.router.navigate(['/login']);
+      this.router.navigate(['']);
       return false;
     }
   }
